67d709149434f8ef2b41d3a91d8f0c3d7fa41e99
diana2341 <dianaponce2341@gmail.com>
2020-04-13 15:31:31 -0400

    did reviews and reservations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources :payments
   resources :reservations
   resources :locations
@@ -7,6 +10,8 @@ Rails.application.routes.draw do
   resources :listings
   resources :users
 
-  # get '/' 'listings#index' as homepage
+get '/reservations/:id/confirmation_page', to: 'reservations#confirmation_page', as:'confirmation_page'
+  get '/login', to: 'sessions#new', as: 'login'
+  get '/logout', to: 'sessions#destroy', as: 'logout'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
