c823ba97109e5fe7829cabf948d22e9eb982fdcb
JohnsonNYC <jkow95@gmail.com>
2020-04-13 16:48:28 -0400

    Completed Basic MVC for User Listings and Locations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
-require 'bcrypt'
+# require 'bcrypt'
 class User < ApplicationRecord
-    include BCryptclea
+    # include BCryptclea
     has_many :listings
     has_many :reservations
     has_many :messages, through: :listings
     belongs_to :review
 
-    has_secure_password
+    # has_secure_password
     validates :email, presence: true, uniqueness: true
 end
