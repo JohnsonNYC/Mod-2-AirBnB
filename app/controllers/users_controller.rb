49c9c4f075ef6eca4a4073b642b533b1c155862f
JohnsonNYC <jkow95@gmail.com>
2020-04-13 14:18:12 -0400

    Creating MVC for Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+    def show 
+        @user = User.find(params[:id])
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        @user.save
+        redirect_to user_path(@user)
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end 
+
+    def update 
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        redirect_to user_path(@user)
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :age, :email, :password, :review_id)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
     has_many :reservations
     has_many :messages, through: :listings
     belongs_to :review
+
+    has_secure_password
+    validates :email, presence: true, uniqueness: true
 end
